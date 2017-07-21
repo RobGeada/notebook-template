2add725e00b9a9a760facdca269bbbdd5b54e685
Rob Geada <rob@geada.net>
2017-07-21 12:59:22 -0500

    actually tested it

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,85 @@
 #!/bin/bash
 
+#========================================================
+#parse arguments
+#========================================================
+DEFAULT_OPENSHIFT_PROJECT=auto-project
+DEFAULT_OPENSHIFT_USER=developer
+
+while getopts :c:u:p:h opt; do
+    case $opt in
+        c)
+            OS_CLUSTER=$OPTARG
+            ;;
+        u)
+            OS_USER=$OPTARG
+            USER_REQUESTED=true
+            ;;
+        p)
+            PROJECT=$OPTARG
+            ;;
+
+        h)
+			echo
+            echo "Usage: deploy.sh [options]"
+            echo
+            echo "Automatic deployment of the Oshinko suite and your notebook/data into OpenShift "
+            echo
+            echo "optional arguments:"
+            echo "  -h            show this help message"
+            echo "  -c CLUSTER    OpenShift cluster url to login against (default: https://localhost:8443)"
+            echo "  -u USER       OpenShift user to run commands as (default: $DEFAULT_OPENSHIFT_USER)"
+            echo "  -p PROJECT    OpenShift project name (default: $DEFAULT_OPENSHIFT_PROJECT)"
+            echo
+            exit
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit
+            ;;
+    esac
+done
+
+if [ -z "$PROJECT" ]
+then
+    echo "project name not set, using default value"
+    PROJECT=$DEFAULT_OPENSHIFT_PROJECT
+fi
+
+if [ -z "$OS_USER" ]
+then
+    echo "openshift user not set, using default value"
+    OS_USER=$DEFAULT_OPENSHIFT_USER
+fi
+
+#========================================================
+#here's the actual script
+#========================================================
+
 #define new project name
-PNAME=autodeploy-$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-z' | fold -w 2 | head -n 1)
+PNAME=${PROJECT}-$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-z' | fold -w 2 | head -n 1)
+
+#get docker username
+username=$(docker info | sed '/Username:/!d;s/.* //')
 
 #delete old projects under same name family
-oc delete project $(oc projects -q | grep autodeploy)
+oc delete project $(oc projects -q | grep ${PROJECT})
 
 #push worker image
 cd spark-worker
-docker build -t rgeada/spark-worker .
-docker push rgeada/spark-worker
+docker build -t ${username}/auto-spark-worker .
+docker push ${username}/auto-spark-worker
 
 #push notebook image
 cd ../notebook
-docker build -t rgeada/notebook .
-docker push rgeada/notebook
+docker build -t ${username}/auto-notebook .
+docker push ${username}/auto-notebook
 
 #deploy oshinko
 cd ..
 oc new-project $PNAME
 oc create configmap default-oshinko-cluster-config --from-file=desired-cluster-config
-./oshinko-deploy.sh -s rgeada/spark-worker -u rgeada -p $PNAME
+./oshinko-deploy.sh -s ${username}/auto-spark-worker -u $OS_USER -p $PNAME
 
 #expose REST server
 oc expose service oshinko-rest
@@ -38,11 +97,11 @@ oc get pods
 curl -H "Content-Type: application/json" -X POST -d '{"name": "sparky", "config": {"workerCount": 10, "masterCount": 1}}' $REST_URL
 
 #add notebook image
-oc new-app rgeada/notebook
+oc new-app ${username}/auto-notebook
 
 #expose route to notebook
-oc expose service notebook
-NOTEBOOK_URL=$(oc get routes | awk '{print $2}' | grep notebook)
+oc expose service auto-notebook
+NOTEBOOK_URL=$(oc get routes | awk '{print $2}' | grep auto-notebook)
 
 #wait until notebook is fully deployed
 while [[ $(oc get pods | awk '/notebook/ && !/deploy/' | awk '{print $2}') != "1/1" ]] 
@@ -51,5 +110,7 @@ while [[ $(oc get pods | awk '/notebook/ && !/deploy/' | awk '{print $2}') != "1
 		sleep 5
 	done
 
+echo "Waiting for Jupyter page to ready up..."
+sleep 10
 open http://$NOTEBOOK_URL
 
